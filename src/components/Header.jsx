5d9c6639cd4701f5f61ce01d6d1f004fdfe614eb
Kitti.Bank <kittitach.pro@gmail.com>
2025-05-12 19:59:10 +0700

    Add header, add components, images

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
+            <div className="max-w-screen-2xl w-full mx-auto px-4">
+                <h1>
+                    <a href="/" className="logo">
+                        <img src="/images/logo.svg" width={40} height={40} alt="Kittitad" />
+                    </a>
+                </h1>
+            </div>
+
+        </header>
+        // <div>Helslo</div>
+    )
+}
+
+export default Header
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react'; //Node nodule
+import { createRoot } from 'react-dom/client'; //Node module
+import './index.css'; //CSS link
+import App from './App.jsx'; //Component
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
